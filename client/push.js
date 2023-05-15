171f934113b0567f0ed826500b9f582ae63d855e
Nguyen Minh Nhat Duong <duongcoilc2004@gmail.com>
2023-05-15 10:53:15 +0700

    write push script

---
+++ client/push.js
@@ -0,0 +1,17 @@
+const { exec } = require("child_process");
+const yargs = require("yargs");
+
+const argv = yargs(process.argv.slice(2)).default("text", "update code").argv;
+
+const text = argv._[0] ? argv._[0] : argv.text;
+
+exec(
+  `cd .. && git add . && git commit -m "${text}" && git push`,
+  (err, stdout, stderr) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(stdout);
+  }
+);
