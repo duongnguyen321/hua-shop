eee4e866ca1caf881507692807f346758b1d7583
Nguyen Minh Nhat Duong <duongcoilc2004@gmail.com>
2023-05-17 16:09:27 +0700

    Update folder structure

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-import Loading from '../Loading'
-
+import React from "react";
+import Section from "../../components/Section";
+import homeStyle from "./assets/styles/home.module.scss";
 export default function Home() {
+  const { home, background } = homeStyle;
   return (
-    <Loading/>
-  )
+    <>
+      <Section className={home}>
+        <div className={background}></div>
+        <h1 className="title">Hello World</h1>
+      </Section>
+      <Section className="home">
+        <h1 className="title">Hello world!</h1>
+      </Section>
+    </>
+  );
 }
--- client/src/pages/Loading/index.jsx
+++ client/src/pages/Loading/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import loadingStyle from "./loading.module.scss";
 export default function Loading() {
-  const { loading, container, dash, uno, dos, tres, cuatro } = loadingStyle;
+  const { loading,title, container, dash, uno, dos, tres, cuatro } = loadingStyle;
   return (
     <div className={loading}>
+      <h1 className={title}>Loading...</h1>
       <div className={container}>
         <div className={`${dash} ${uno}`}></div>
         <div className={`${dash} ${dos}`}></div>
