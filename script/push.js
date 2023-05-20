f6c11fd2aa2a364b88de458c1163d037bda26075
Nguyen Minh Nhat Duong <duongcoilc2004@gmail.com>
2023-05-20 23:28:06 +0700

    updte script with vercel

--- script/push.js
+++ script/push.js
@@ -5,13 +5,39 @@ const argv = yargs(process.argv.slice(2)).default("text", "update code").argv;
 
 const text = argv._[0] ? argv._[0] : argv.text;
 
-exec(
-  `git add . && git commit -m "${text}" && git push`,
-  (err, stdout, stderr) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(stdout);
+const pushChild = exec(`git add . && git commit -m "${text}" && git push`);
+
+pushChild.stdout.on("data", (data) => {
+  console.log(data);
+});
+
+pushChild.stderr.on("data", (data) => {
+  console.error(data);
+});
+
+pushChild.on("error", (error) => {
+  console.error(error);
+});
+
+pushChild.on("close", (code) => {
+  console.log(`Git push process exited with code ${code}`);
+  if (code === 0) {
+    const vercelChild = exec("vercel --prod");
+
+    vercelChild.stdout.on("data", (data) => {
+      console.log(data);
+    });
+
+    vercelChild.stderr.on("data", (data) => {
+      console.error(data);
+    });
+
+    vercelChild.on("error", (error) => {
+      console.error(error);
+    });
+
+    vercelChild.on("close", (code) => {
+      console.log(`Vercel process exited with code ${code}`);
+    });
   }
-);
+});
