5e30b027c9b7583ab9b01dfdb69beb0b9ce2ca52
duongnguyenf8 <nguyenduong@fullstack.edu.vn>
2023-07-15 12:12:07 +0700

    Update Theme Light with 3 lines code

--- src/Layout/Header/components/Icons.jsx
+++ src/Layout/Header/components/Icons.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useCallback, useLayoutEffect, useState } from "react";
 import Links from "../../../helper/Link";
 export default function Icons({ icons, icon, cart, profile }) {
+  const [theme, setTheme] = useState("dark");
+  const handleChangeTheme = useCallback(() => {
+    setTheme((theme) => (theme === "dark" ? "light" : "dark"));
+    localStorage.theme = theme;
+    document.querySelector("body").className = theme;
+  }, [theme]);
+  useLayoutEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+      setTheme(savedTheme);
+      document.querySelector("body").className = savedTheme;
+    }
+  }, []);
   return (
     <div className={icons}>
+      <i
+        className={`${icon} fa ${theme === "light" ? "fa-moon" : "fa-sun"}`}
+        onClick={handleChangeTheme}
+      ></i>
       <Links to={cart} className={icon}>
         <i className="fas fa-cart-shopping"></i>
       </Links>
